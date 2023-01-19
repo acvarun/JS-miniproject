803a77346d77b4f498635ff13d326f4d889722c0
acvarun <96919265+acvarun@users.noreply.github.com>
2023-01-19 21:02:48 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
-const pokemon={
-    Charmander:{
+const pokemon=[
+    {
         id:0,
-        name : "Charmander",
-        type : "fire",
-        image : "https://w7.pngwing.com/pngs/156/686/png-transparent-pokemon-go-pokemon-x-and-y-ash-ketchum-charmander-pokemon-background-orange-cartoon-fictional-character-thumbnail.png", 
-        attack1 : "ember",
+        name : "Charizard",
+        type : "Fire",
+        image : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png", 
+        attack1 : "flame-thrower",
         value1: 10,
-        attack2 : "metal-claw",
+        attack2 : "ember",
         value2: 8,
         attack3 : "scratch",
         value3: 9,
-        attack4 : "flame-thrower",
+        attack4 : "hyper-beam",
         value4: 15
     }
     ,
-    Bulbasaur:{
+    {
         id : 1,
-        name : "Bulbasaur",
-        type : "grass",
-        image : "https://w7.pngwing.com/pngs/957/190/png-transparent-green-pokemon-character-pokemon-red-and-blue-ash-ketchum-bulbasaur-wikia-pokemon-marine-mammal-mammal-vertebrate-thumbnail.png",
+        name : "Venusaur",
+        type : "Grass",
+        image : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png",
         attack1 : "vinewhip",
         value1 : 10,
         attack2 : "rasor-leaf",
@@ -29,26 +29,26 @@ const pokemon={
         value4 : 15
     }
     ,
-    Squirtle:{
+    {
         id:2,
-        name : "Squirtle",
-        type : "water",
-        image : "https://e7.pngegg.com/pngimages/700/421/png-clipart-pokemon-squirtle-squirtle-pokemon-games-pokemon-thumbnail.png",
+        name : "Blastoise",
+        type : "Water",
+        image : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png",
         attack1 : "tackle",
         value1 : 8,
-        attack2 : "water-gun",
+        attack2 : "hydro-cannon",
         value2 : 10,
-        attack3 : "tail-whip",
+        attack3 : "water-gun",
         value3 : 9,
         attack4 : "hydro-pump",
         value4 : 15
     }
     ,
-    Pikachu:{
+    {
         id : 3,
-        name : "Pikachu",
-        type : "electric",
-        image : "https://w7.pngwing.com/pngs/585/436/png-transparent-pokemon-pikachu-illustration-icon-pikachu-background-mammal-food-vertebrate-thumbnail.png",
+        name : "Raichu",
+        type : "Electric",
+        image : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/026.png",
         attack1 : "tackle",
         value1 : 8,
         attack2 : "thunder-bolt",
@@ -58,11 +58,42 @@ const pokemon={
         attack4 : "thunder-shock",
         value4 : 10
     }
-}
+    ,
+    {
+        id : 4,
+        name : "Pidgeot",
+        type : "Flying",
+        image : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/018.png",
+        attack1 : "peck",
+        value1 : 8,
+        attack2 : "wing-attack",
+        value2 : 15,
+        attack3 : "fly",
+        value3 : 9,
+        attack4 : "steel-wing",
+        value4 : 10
+    }
+    ,
+    {
+        id : 5,
+        name : "Dragonite",
+        type : "Dragon",
+        image : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/149.png",
+        attack1 : "dragon-claw",
+        value1 : 8,
+        attack2 : "dragon-breath",
+        value2 : 15,
+        attack3 : "steel-wing",
+        value3 : 9,
+        attack4 : "hyper-beam",
+        value4 : 10
+    }
 
-let obj1=pokemon.Charmander
-let input1=document.getElementsByClassName("slider")[0];
-let input2=document.getElementsByClassName("slider")[1];
+]
+console.log(pokemon[0].id)
+let obj1=pokemon[0]
+let input1=document.getElementById("input1");
+let input2=document.getElementById("input2");
 let button1=document.getElementById("button1");
 let button2=document.getElementById("button2");
 let button3=document.getElementById("button3");
@@ -84,113 +115,61 @@ value14=obj1.value4;
 
 
 
-let selected1=document.getElementById("pokemon1").value;
+let selected1="Yours Charizard";
 
 function handleChange1(){
-    let select1=document.getElementById("pokemon1").value;
-    if(select1===pokemon.Charmander.name){
-        obj1=pokemon.Charmander
-        selected1=obj1.name
-        document.getElementById("img1").src=obj1.image;
-        document.getElementById("type1").innerHTML=obj1.type;
-        button1.innerHTML=obj1.attack1;
-        button2.innerHTML=obj1.attack2;
-        button3.innerHTML=obj1.attack3;
-        button4.innerHTML=obj1.attack4;
-        value11=obj1.value1;
-        value12=obj1.value2;
-        value13=obj1.value3;
-        value14=obj1.value4;
+    selected1=document.getElementById("pokemon1").value;
+
+    for(let i=0;i<pokemon.length;i++){
+        if(pokemon[i].name===selected1){
+            obj1=pokemon[i]
+            selected1="Yours "+obj1.name;
+            console.log(selected1)
+            document.getElementById("img1").src=obj1.image;
+            document.getElementById("type1").innerHTML=obj1.type;
+            document.getElementById("health-img1").src=obj1.image;
+            document.getElementById("healthbar-name1").innerHTML=obj1.name;
+            button1.innerHTML=obj1.attack1;
+            button2.innerHTML=obj1.attack2;
+            button3.innerHTML=obj1.attack3;
+            button4.innerHTML=obj1.attack4;
+            value11=obj1.value1;
+            value12=obj1.value2;
+            value13=obj1.value3;
+            value14=obj1.value4;
+            break;
+        }
     }
-    else if(select1===pokemon.Bulbasaur.name){
-        obj1=pokemon.Bulbasaur
-        selected1=obj1.name
-        document.getElementById("img1").src=obj1.image;
-        document.getElementById("type1").innerText=obj1.type;
-        button1.innerHTML=obj1.attack1;
-        button2.innerHTML=obj1.attack2;
-        button3.innerHTML=obj1.attack3;
-        button4.innerHTML=obj1.attack4;
-        value11=obj1.value1;
-        value12=obj1.value2;
-        value13=obj1.value3;
-        value14=obj1.value4;
-    }
-    else if(select1===pokemon.Squirtle.name){
-        obj1=pokemon.Squirtle
-        selected1=obj1.name
-        document.getElementById("img1").src=obj1.image;
-        document.getElementById("type1").innerText=obj1.type;
-        button1.innerHTML=obj1.attack1;
-        button2.innerHTML=obj1.attack2;
-        button3.innerHTML=obj1.attack3;
-        button4.innerHTML=obj1.attack4;
-        value11=obj1.value1;
-        value12=obj1.value2;
-        value13=obj1.value3;
-        value14=obj1.value4;
-    }
-    else{
-        obj1=pokemon.Pikachu
-        selected1=obj1.name
-        document.getElementById("img1").src=obj1.image;
-        document.getElementById("type1").innerText=obj1.type;
-        button1.innerHTML=obj1.attack1;
-        button2.innerHTML=obj1.attack2;
-        button3.innerHTML=obj1.attack3;
-        button4.innerHTML=obj1.attack4;
-        value11=obj1.value1;
-        value12=obj1.value2;
-        value13=obj1.value3;
-        value14=obj1.value4;
-    }
+    
 }
 let obj2={}
 let selected2="";
 let num=null;
 function opponent(){
-    num=Math.floor(Math.random()*4)
-    if(num===pokemon.Charmander.id){
-        obj2=pokemon.Charmander;
-        selected2=obj2.name
-        document.getElementById("pokemon2").innerHTML=obj2.name;
-        document.getElementById("img2").src=obj2.image;
-        document.getElementById("type2").innerHTML=obj2.type;
+    num=Math.floor(Math.random()*(pokemon.length+1))
+    for(let i=0;i<pokemon.length;i++){
+        if(pokemon[i].id===num){
+            obj2=pokemon[i];
+            selected2="Opponent "+obj2.name
+            document.getElementById("pokemon2").innerHTML=obj2.name;
+            document.getElementById("img2").src=obj2.image;
+            document.getElementById("type2").innerHTML=obj2.type;
+            document.getElementById("healthbar-name2").innerHTML=obj2.name;
+            document.getElementById("health-img2").src=obj2.image;
+            break;
+        }
     }
-    else if(num===pokemon.Bulbasaur.id){
-        obj2=pokemon.Bulbasaur;
-        selected2=obj2.name
-        document.getElementById("pokemon2").innerHTML=obj2.name;
-        document.getElementById("img2").src=obj2.image;
-        document.getElementById("type2").innerHTML=obj2.type;
-    }
-    else if(num===pokemon.Squirtle.id){
-        obj2=pokemon.Squirtle;
-        selected2=obj2.name
-        document.getElementById("pokemon2").innerHTML=obj2.name;
-        document.getElementById("img2").src=obj2.image;
-        document.getElementById("type2").innerHTML=obj2.type;
-        
-    }
-    else{
-        obj2=pokemon.Pikachu;
-        selected2=obj2.name
-        document.getElementById("pokemon2").innerHTML=obj2.name;
-        document.getElementById("img2").src=obj2.image;
-        document.getElementById("type2").innerHTML=obj2.type;
-    }
-    
 }
 
 
 function handleClick(){
-    if(num==null){
+    if(num===null){
         document.getElementById("warning").style.display="block"
     }
     else{
         document.getElementById("selection").style.display="none";
         document.getElementById("match").style.display="block";
-        document.getElementById("pokemonLogo").style.height="150px";
+        document.getElementById("pokemonLogo").style.display="none";
     }
 }
 
@@ -217,11 +196,11 @@ function handleClick1(){
     button2.style.visibility="hidden";
     button3.style.visibility="hidden";
     button4.style.visibility="hidden";
-    document.getElementById("poke1").innerHTML=obj1.name;
-    document.getElementById("atk1").innerHTML=obj1.attack1;
+    document.getElementById("poke").innerHTML=obj1.name;
+    document.getElementById("atk").innerHTML=obj1.attack1;
     document.getElementById("damage2").innerHTML=-1*obj1.value1;
     document.getElementById("nextBtn").style.visibility="visible"
-    document.getElementById("atk").style.visibility="visible"
+    document.getElementById("atkDiv").style.visibility="visible"
 }
 
 function handleClick2(){
@@ -242,11 +221,11 @@ function handleClick2(){
     button2.style.visibility="hidden";
     button3.style.visibility="hidden";
     button4.style.visibility="hidden";
-    document.getElementById("poke1").innerHTML=obj1.name;
-    document.getElementById("atk1").innerHTML=obj1.attack2;
+    document.getElementById("poke").innerHTML=obj1.name;
+    document.getElementById("atk").innerHTML=obj1.attack2;
     document.getElementById("damage2").innerHTML=-1*obj1.value2;
     document.getElementById("nextBtn").style.visibility="visible"
-    document.getElementById("atk").style.visibility="visible"
+    document.getElementById("atkDiv").style.visibility="visible"
 }
 
 function handleClick3(){
@@ -267,11 +246,11 @@ function handleClick3(){
     button2.style.visibility="hidden";
     button3.style.visibility="hidden";
     button4.style.visibility="hidden";
-    document.getElementById("poke1").innerHTML=obj1.name;
-    document.getElementById("atk1").innerHTML=obj1.attack3;
+    document.getElementById("poke").innerHTML=obj1.name;
+    document.getElementById("atk").innerHTML=obj1.attack3;
     document.getElementById("damage2").innerHTML=-1*obj1.value3;
     document.getElementById("nextBtn").style.visibility="visible"
-    document.getElementById("atk").style.visibility="visible"
+    document.getElementById("atkDiv").style.visibility="visible"
 }
 
 function handleClick4(){
@@ -292,21 +271,22 @@ function handleClick4(){
     button2.style.visibility="hidden";
     button3.style.visibility="hidden";
     button4.style.visibility="hidden";
-    document.getElementById("poke1").innerHTML=obj1.name;
-    document.getElementById("atk1").innerHTML=obj1.attack4;
+    document.getElementById("poke").innerHTML=obj1.name;
+    document.getElementById("atk").innerHTML=obj1.attack4;
     document.getElementById("damage2").innerHTML=-1*obj1.value4;
     document.getElementById("nextBtn").style.visibility="visible"
-    document.getElementById("atk").style.visibility="visible"
+    document.getElementById("atkDiv").style.visibility="visible"
 }
 
 function next(){
-    document.getElementById("oppAtk").style.visibility="visible"
     let n=Math.floor(Math.random()*4)
-    if(n==0){
-        document.getElementById("poke2").innerHTML=obj2.name;
-        document.getElementById("atk2").innerHTML=obj2.attack1;
+    console.log(n)
+    console.log(obj2.name)
+    if(n===0){
+        document.getElementById("poke").innerHTML=obj2.name;
+        document.getElementById("atk").innerHTML=obj2.attack1;
         document.getElementById("damage1").innerHTML=-1*obj2.value1;
-        result1=result1-obj2.value1
+        result1=result1-obj2.value1;
         if(result1<=0){
             loser=selected1;
             winner=selected2
@@ -320,11 +300,11 @@ function next(){
             document.getElementById("health1").innerHTML=result1;
         }
     }
-    else if(n==1){
-        document.getElementById("poke2").innerHTML=obj2.name;
-        document.getElementById("atk2").innerHTML=obj2.attack2;
+    else if(n===1){
+        document.getElementById("poke").innerHTML=obj2.name;
+        document.getElementById("atk").innerHTML=obj2.attack2;
         document.getElementById("damage1").innerHTML=-1*obj2.value2;
-        result1=result1-obj2.value2
+        result1=result1-obj2.value2;
         if(result1<=0){
             loser=selected1;
             winner=selected2
@@ -338,11 +318,11 @@ function next(){
             document.getElementById("health1").innerHTML=result1;
         }
     }
-    else if(n==2){
-        document.getElementById("poke2").innerHTML=obj2.name;
-        document.getElementById("atk2").innerHTML=obj2.attack3;
+    else if(n===2){
+        document.getElementById("poke").innerHTML=obj2.name;
+        document.getElementById("atk").innerHTML=obj2.attack3;
         document.getElementById("damage1").innerHTML=-1*obj2.value3;
-        result1=result1-obj2.value3
+        result1=result1-obj2.value3;
         if(result1<=0){
             loser=selected1;
             winner=selected2
@@ -356,11 +336,11 @@ function next(){
             document.getElementById("health1").innerHTML=result1;
         }
     }
-    else{
-        document.getElementById("poke2").innerHTML=obj2.name;
-        document.getElementById("atk2").innerHTML=obj2.attack4;
+    else if(n===3){
+        document.getElementById("poke").innerHTML=obj2.name;
+        document.getElementById("atk").innerHTML=obj2.attack4;
         document.getElementById("damage1").innerHTML=-1*obj2.value4;
-        result1=result1-obj2.value4
+        result1=result1-obj2.value4;
         if(result1<=0){
             loser=selected1;
             winner=selected2
@@ -384,12 +364,12 @@ function next(){
 function endMatch(){
     console.log(selected1)
     console.log(selected2)
-    if(loser==selected1){
-        document.getElementById("result").innerHTML=obj2.name;
+    if(winner===selected2){
+        document.getElementById("winner-span").innerHTML=selected2;
         document.getElementById("winner-img").src=obj2.image
     }
-    else{
-        document.getElementById("result").innerHTML=obj1.name
+    else if(winner===selected1){
+        document.getElementById("winner-span").innerHTML=selected1
         document.getElementById("winner-img").src=obj1.image
     }
     document.getElementById("result").style.display="block"
